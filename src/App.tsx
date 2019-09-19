1a3ea1b6bb6b255be45d0b1375cf89a0ed1c03e8
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-09-19 17:21:22 +0200

    Add minimal example how to talk to HTTP-JSON API

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  handleClick = () => {
+    const body = {"%templates": [{"moduleName": "Simplified", "entityName": "Expense"}]};
+    fetch('contracts/search', {
+        body: JSON.stringify(body),
+        headers: {
+          'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsZWRnZXJJZCI6InphbWxpdmUiLCJhcHBsaWNhdGlvbklkIjoiemFtbGl2ZSIsInBhcnR5IjoiQWxpY2UifQ.agRMcyhpDBE7rxs5dzlDQIdl6F9qBLAryt8sKvlj_ek',
+          'Content-type': 'application/json'
+        },
+        method: 'post',
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      alert('Success!\n' + data.toString());
+    })
+    .catch((error) => {
+      alert('Failure!\n' + error);
+    });
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button onClick={this.handleClick}>Click me!</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
