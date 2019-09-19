20ebdf33d5264079d8f977371fcac89a9a8a4bd4
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-09-19 17:31:43 +0200

    Fix authorization for JSON API

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends React.Component {
   handleClick = () => {
     const body = {"%templates": [{"moduleName": "Simplified", "entityName": "Expense"}]};
     fetch('contracts/search', {
         body: JSON.stringify(body),
         headers: {
-          'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsZWRnZXJJZCI6InphbWxpdmUiLCJhcHBsaWNhdGlvbklkIjoiemFtbGl2ZSIsInBhcnR5IjoiQWxpY2UifQ.agRMcyhpDBE7rxs5dzlDQIdl6F9qBLAryt8sKvlj_ek',
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsZWRnZXJJZCI6InphbWxpdmUiLCJhcHBsaWNhdGlvbklkIjoiemFtbGl2ZSIsInBhcnR5IjoiQWxpY2UifQ.agRMcyhpDBE7rxs5dzlDQIdl6F9qBLAryt8sKvlj_ek',
           'Content-type': 'application/json'
         },
         method: 'post',
     })
     .then((response) => response.json())
     .then((data) => {
-      alert('Success!\n' + data.toString());
+      alert('Success!\n' + JSON.stringify(data));
     })
     .catch((error) => {
       alert('Failure!\n' + error);
