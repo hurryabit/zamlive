4c28d173d1f93774c1e50288452492c632c495ab
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-09-20 10:19:54 +0200

    Prepare login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,17 @@
 import React from 'react';
-import AccountsList from './AccountsList';
-import ApprovalTab from './ApprovalTab';
-import CreateTab from './CreateTab';
+import Main from './Main';
 import User from './User';
 import './App.css';
 
-type Props = {}
-
-type Mode = 'Accounts' | 'Create' | 'Approvals';
-
-type State = {
-  mode: Mode;
-}
-
-class App extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      mode: 'Accounts'
-    }
-  }
-
-  setMode = (mode: Mode) => {
-    this.setState({mode});
-  }
-
-  renderAccounts(user: User) {
-    return <AccountsList user={user} />;
-  }
-
-  renderCreate() {
-    return <CreateTab />;
-  }
-
-  renderApprovals(user: User) {
-    return <ApprovalTab user={user} />;
-  }
-
-  render () {
-    let user = {
+class App extends React.Component {
+  render() {
+    let user: User = {
       name: 'Martin',
       password: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsZWRnZXJJZCI6InphbWxpdmUiLCJhcHBsaWNhdGlvbklkIjoiemFtbGl2ZSIsInBhcnR5IjoiTWFydGluIn0.uYdP2MNhOcw-zYF-_tOxZygqQldhRBgX0rCTfUgZ4A4'
     }
-    let body;
-    switch (this.state.mode) {
-      case 'Accounts':
-        body = this.renderAccounts(user);
-        break;
-      case 'Create':
-        body = this.renderCreate();
-        break;
-      case 'Approvals':
-        body = this.renderApprovals(user);
-        break;
-    }
+
     return (
-      <div>
-        <h1>ZAMLive</h1>
-        <hr />
-        <div>
-          <button onClick={() => this.setMode('Accounts')}>Accounts</button>
-          <button onClick={() => this.setMode('Create')}>Create Account</button>
-          <button onClick={() => this.setMode('Approvals')}>Approvals</button>
-        </div>
-        <hr />
-        {body}
-      </div>
+      <Main user={user} />
     );
   }
 }
