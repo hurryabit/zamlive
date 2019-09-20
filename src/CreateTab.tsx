a0d66abda30d7857ee69dd9db74db62eef773fa6
Remy <remy.haemmerle@daml.com>
2019-09-20 14:50:53 +0200

    show account proposale creation status (#44)

--- src/CreateTab.tsx
+++ src/CreateTab.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import User from "./User";
+import Ledger from "./Ledger";
 
 type Props = {
     user: User;
 }
 
+type Status = 'Ok' | 'Fail' | 'Unknown'
+
 type State = {
     members: string[];
     name: string;
+    status: Status;
 }
 
 class CreateTab extends React.Component<Props, State> {
 
-    private users = ["martin", "oliver", "remy", "jussi"];
+    private users = ["Martin", "Oliver", "Remy", "Jussi"];
 
     constructor(props: Props) {
         super(props);
 
         this.state = {
             members: [this.props.user.name],
-            name: "My Awesome Group"
+            name: "My Awesome Group",
+            status: 'Unknown',
         };
     }
+
+
 
     handleMembersUpdate = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const options = event.target.options;
@@ -33,16 +40,37 @@ class CreateTab extends React.Component<Props, State> {
                 members.push(options[i].value);
         }
 
-        this.setState({members: members});
+        this.setState({members: members, status: 'Unknown'});
     };
 
     handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({name: event.target.value})
+        this.setState({name: event.target.value, status: 'Unknown'})
     };
 
     handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        const user = this.props.user.name;
+        const payload = {
+            templateId: {
+                moduleName: "Main",
+                entityName: "AccountProposal"
+            },
+            argument: {
+                pendingMembers:
+                    this.state.members.filter((value, _1, _2) => value !== user),
+                members: [user],
+                name: this.state.name,
+            }
+        };
+        console.log(payload);
+        Ledger.fetch(this.props.user, 'command/create', payload).then(
+            x => {
+                console.log(x)
+                this.setState({status: 'Ok'})},
+            _ => this.setState({status: 'Fail'})
+        )
     };
+
 
     render() {
         return (
@@ -56,11 +84,20 @@ class CreateTab extends React.Component<Props, State> {
                 <label>Group Members
                     <select multiple={true} value={this.state.members}
                             onChange={this.handleMembersUpdate}>
-                        {this.users.map((user) => <option key={user} value={user}>{user}</option>)}
+                        {this.users.map((user) => <option key={user}
+                                                          value={user}>{user}</option>)}
                     </select>
                 </label>
                 <br/>
-                <input type='submit' value='Submit' />
+                <input type='submit' value='Submit'/>
+                <br/><br/>
+                {
+                    (this.state.status === 'Ok') ?
+                        <div style={{color:"green", fontWeight: "bold"}}>Account Proposal created successfully</div> :
+                        (this.state.status === 'Fail') ?
+                            <div style={{color:"red", fontWeight: "bold"}}>Account Proposal creation fails</div> :
+                            <div/>
+                }
             </form>
         );
     }
