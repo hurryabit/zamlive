b45e2312aece332c880182760b5d5a3eda57ce8d
Remy Haemmerle <Remy.Haemmerle@digitalasset.com>
2019-09-20 13:18:42 +0200

    finish form to Create Account

--- src/CreateTab.tsx
+++ src/CreateTab.tsx
@@ -5,18 +5,63 @@ type Props = {
     user: User;
 }
 
-class CreateTab extends React.Component<Props> {
+type State = {
+    members: string[];
+    name: string;
+}
 
-    private users = ["Martin", "Oliver", "Remy", "Jussi"];
+class CreateTab extends React.Component<Props, State> {
+
+    private users = ["martin", "oliver", "remy", "jussi"];
+
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            members: [this.props.user.name],
+            name: "My Awesome Group"
+        };
+    }
+
+    handleMembersUpdate = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const options = event.target.options;
+        const length = options.length;
+        const user = this.props.user.name;
+        var members = [];
+        for (let i = 0; i < length; i++) {
+            if (options[i].selected || options[i].value === user)
+                members.push(options[i].value);
+        }
+
+        this.setState({members: members});
+    };
+
+    handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({name: event.target.value})
+    };
+
+    handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    };
 
     render() {
         return (
-            <div>
+            <form onSubmit={this.handleSubmit}>
                 <h2>Create Account</h2>
-                <select multiple={true}>
-                    {(this.users).filter((user, _1, _2) => user !== this.props.user.name).map((user) => <option value ={user}>{user}</option>)}
-                </select>
-            </div>
+                <label>Group Name
+                    <input type='text' value={this.state.name}
+                           onChange={this.handleNameChange}/>
+                </label>
+                <br/>
+                <label>Group Members
+                    <select multiple={true} value={this.state.members}
+                            onChange={this.handleMembersUpdate}>
+                        {this.users.map((user) => <option key={user} value={user}>{user}</option>)}
+                    </select>
+                </label>
+                <br/>
+                <input type='submit' value='Submit' />
+            </form>
         );
     }
 }
