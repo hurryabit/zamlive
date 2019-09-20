761c46986060657839d1148b9d15bc42fa4ec308
Oliver Seeliger <oliver.seeliger@digitalasset.com>
2019-09-20 14:56:38 +0200

    Add account name to expenses

--- src/ExpenseNotification.ts
+++ src/ExpenseNotification.ts
@@ -6,16 +6,19 @@ export class ExpenseNotification {
   payer: string = '';
   beneficiaries: string[] = [];
   amount: number = 0.0;
+  accountName: string = '';
 
   static fromJson(obj: any): ExpenseNotification {
-    let res: ExpenseNotification = new ExpenseNotification
+    let res: ExpenseNotification = new ExpenseNotification()
     res.templateId = obj.templateId;
     res.contractId = obj.contractId;
     res.approver = obj.argument.approver;
+    console.log(obj.argument.expense)
     res.description = obj.argument.expense.description;
     res.payer = obj.argument.expense.payer;
     res.beneficiaries = obj.argument.expense.beneficiaries;
     res.amount = obj.argument.expense.amount;
+    res.accountName = obj.argument.expense.account._2;
 
     return res;
   }
