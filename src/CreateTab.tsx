897ad7003515aa2cb746a15df44824e598933449
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-09-20 15:49:36 +0200

    Style create form using bootstrap (#51)

--- src/CreateTab.tsx
+++ src/CreateTab.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Col, Container, FormControl, FormControlProps, Row, Form } from 'react-bootstrap';
 import User from "./User";
 import Ledger from "./Ledger";
 
@@ -47,12 +48,10 @@ class CreateTab extends React.Component<Props, State> {
 
         this.state = {
             members: [this.props.user.name],
-            name: "My Awesome Group",
+            name: '',
             status: 'Unknown',
         };
     }
-
-
 
     handleMembersUpdate = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const options = event.target.options;
@@ -67,8 +66,8 @@ class CreateTab extends React.Component<Props, State> {
         this.setState({members: members, status: 'Unknown'});
     };
 
-    handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({name: event.target.value, status: 'Unknown'})
+    handleNameChange = (event: React.FormEvent<FormControlProps & FormControl>) => {
+        this.setState({name: event.currentTarget.value || '', status: 'Unknown'})
     };
 
     handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -98,31 +97,53 @@ class CreateTab extends React.Component<Props, State> {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <h2>Create Account</h2>
-                <label>Group Name
-                    <input type='text' value={this.state.name}
-                           onChange={this.handleNameChange}/>
-                </label>
-                <br/>
-                <label>Group Members
-                    <select multiple={true} value={this.state.members}
-                            onChange={this.handleMembersUpdate}>
-                        {this.users.map((user) => <option key={user}
-                                                          value={user}>{user}</option>)}
-                    </select>
-                </label>
-                <br/>
-                <input type='submit' value='Submit'/>
-                <br/><br/>
-                {
-                    (this.state.status === 'Ok') ?
-                        <div style={{color:"green", fontWeight: "bold"}}>Account Proposal created successfully</div> :
-                        (this.state.status === 'Fail') ?
-                            <div style={{color:"red", fontWeight: "bold"}}>Account Proposal creation fails</div> :
-                            <div/>
-                }
-            </form>
+            <Container>
+                <Row>
+                    <Col>
+                        <h1 className='text-center'>Create Account</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    </Col>
+                    <Col>
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Group>
+                                <Form.Label>Account Name</Form.Label>
+                                <Form.Control
+                                    type='text'
+                                    value={this.state.name}
+                                    onChange={this.handleNameChange}
+                                />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Members</Form.Label>
+                                <select
+                                    className="form-control"
+                                    multiple
+                                    value={this.state.members}
+                                    onChange={this.handleMembersUpdate}
+                                >
+                                    {this.users.map((user) =>
+                                        <option key={user} value={user}>{user}</option>
+                                    )}
+                                </select>
+                            </Form.Group>
+                            <Button type="submit" className='btn-block'>Submit</Button>
+                            <br/><br/>
+                            {
+                                (this.state.status === 'Ok') ?
+                                    <div style={{color:"green", fontWeight: "bold"}}>Account Proposal created successfully</div> :
+                                    (this.state.status === 'Fail') ?
+                                        <div style={{color:"red", fontWeight: "bold"}}>Account Proposal creation fails</div> :
+                                        <div/>
+                            }
+                        </Form>
+                    </Col>
+                    <Col>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
