def0443e6986c08278b762b3587ed8a09171dfe2
Oliver Seeliger <oliver.seeliger@digitalasset.com>
2019-09-20 13:11:24 +0200

    Account approval UI

--- src/AccountProposal.ts
+++ src/AccountProposal.ts
@@ -1,22 +1,20 @@
-import User from './User';
-
 export class AccountProposal {
   templateId: any = {};
   contractId: string = '';
-  pendingMembers: User[] = [];
-  members: User[] = [];
   name: string = '';
+  pendingMembers: string[] = [];
+  members: string[] = [];
 
   static fromJson(obj: any): AccountProposal {
     let res: AccountProposal = new AccountProposal();
     res.templateId = obj.templateId;
     res.contractId = obj.contractId;
-    res.pendingMembers = obj.pendingMembers;
-    res.members = obj.members;
-    res.name = obj.name;
+    res.pendingMembers = obj.argument.pendingMembers;
+    res.members = obj.argument.members;
+    res.name = obj.argument.name;
 
     return res;
   }
-
-
 }
+
+export default AccountProposal;
