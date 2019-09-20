6077beb718504a1cc95b0180bc883bf124901a25
Oliver Seeliger <46452754+oliverse-da@users.noreply.github.com>
2019-09-20 14:31:51 +0200

    Approval for expenses UI (#43)

--- src/ApprovalTab.tsx
+++ src/ApprovalTab.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AccountProposal from './AccountProposal';
+import ExpenseNotification from './ExpenseNotification';
 import Ledger from './Ledger';
 import User from './User';
 
@@ -7,12 +8,9 @@ type Props = {
   user: User;
 }
 
-type Mode = 'List' | 'Details'
-
 type State = {
-  mode: Mode;
-  approvals: AccountProposal[];
-  plain: string;
+  accountApprovals: AccountProposal[];
+  expensesToApprove: ExpenseNotification[];
 }
 
 class ApprovalTab extends React.Component<Props, State> {
@@ -20,9 +18,8 @@ class ApprovalTab extends React.Component<Props, State> {
     super(props);
 
     this.state = {
-      mode: 'List',
-      approvals: [],
-      plain: ''
+      accountApprovals: [],
+      expensesToApprove: []
     };
   }
 
@@ -41,9 +38,25 @@ class ApprovalTab extends React.Component<Props, State> {
     })
   }
 
+  handleApproveExpense = (expenseNotification: ExpenseNotification) => {
+    const approver = this.props.user.name
+    const payload = {
+      templateId: expenseNotification.templateId,
+      contractId: expenseNotification.contractId,
+      choice: 'Sign',
+      argument: { approver }
+    };
+    Ledger.fetch(this.props.user, 'command/exercise', payload)
+    .then(() => {
+      alert('Expense successfully approved')
+      this.remount()
+    })
+  }
+
   remount() {
-    const payload = {"%templates": [{"moduleName": "Main", "entityName": "AccountProposal"}]};
-    Ledger.fetch(this.props.user, 'contracts/search', payload)
+    // Fetch accounts to approve
+    const fetchAccountProposals = {"%templates": [{"moduleName": "Main", "entityName": "AccountProposal"}]};
+    Ledger.fetch(this.props.user, 'contracts/search', fetchAccountProposals)
     .then(({result}) => {
       let approvals: AccountProposal[] = [];
       for (let workflow of result) {
@@ -54,9 +67,24 @@ class ApprovalTab extends React.Component<Props, State> {
           }
         }
       }
-      this.setState({approvals, plain: JSON.stringify(result)});
+      this.setState({accountApprovals: approvals});
     })
 
+    // Fetch expenses to approve
+    const fetchExpenses = {"%templates": [{"moduleName": "Main", "entityName": "ExpenseNotification"}]};
+    Ledger.fetch(this.props.user, 'contracts/search', fetchExpenses)
+    .then(({result}) => {
+      let expenseNotifications: ExpenseNotification[] = [];
+      for (let workflow of result) {
+        for (let expenseToApprove of workflow.activeContracts) {
+          let expenseNotification: ExpenseNotification = ExpenseNotification.fromJson(expenseToApprove);
+          if (expenseNotification.approver == this.props.user.name) {
+            expenseNotifications.push(expenseNotification);
+          }
+        }
+      }
+      this.setState({expensesToApprove: expenseNotifications});
+    })
   }
 
   componentDidMount() {
@@ -67,10 +95,17 @@ class ApprovalTab extends React.Component<Props, State> {
     return (
       <div>
         <h2>Approvals</h2>
+        <h4>Accounts</h4>
         <ul>
-          {this.state.approvals.map((proposal, index) =>
+          {this.state.accountApprovals.map((proposal, index) =>
             <li key={index}><button onClick={() => this.handleApproveAccountProposal(proposal)}>{proposal.name}</button></li>)}
         </ul>
+        <h4>Expenses</h4>
+        <table>
+          <tr><th>Amount</th><th>Payer</th><th>Purpose</th></tr>
+          {this.state.expensesToApprove.map((expenseNotification, index) =>
+            <tr><td>{expenseNotification.amount}</td><td>{expenseNotification.payer}</td><td><button onClick={() => this.handleApproveExpense(expenseNotification)}>{expenseNotification.description}</button></td></tr>)}
+        </table>
       </div>
     )
   }
